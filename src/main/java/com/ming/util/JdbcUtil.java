084e5fed74c04557e64b1a166b6f4686efabb070
liming <522724933@qq.com>
2020-11-12 21:18:51 +0800

    jdbc

--- src/main/java/com/ming/util/JdbcUtil.java
+++ src/main/java/com/ming/util/JdbcUtil.java
@@ -13,16 +13,16 @@ public class JdbcUtil {
         //2.获取连接
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/test","liming","liming");
         //3.获取操作数据库的预处理对象
-        PreparedStatement preparedStatement = con.prepareStatement("select * from account ");
+        PreparedStatement psm = con.prepareStatement("select * from account ");
         //4.执行sql语句，得到结果集
-        ResultSet resultSet = preparedStatement.executeQuery();
+        ResultSet result = psm.executeQuery();
         //5.封装结果集或遍历结果集
-        while (resultSet.next()){
-            System.out.println(resultSet.getString("name"));
+        while (result.next()){
+            System.out.println(result.getString("name"));
         }
         //6.释放资源
-        resultSet.close();
-        preparedStatement.close();
+        result.close();
+        psm.close();
         con.close();
     }
 }
